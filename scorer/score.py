48ade1e3467ce76860787451678d4882215f2f75
mduval <maxime@kili-technology.com>
2021-02-25 20:58:09 +0100

    Fix score

--- scorer/score.py
+++ scorer/score.py
@@ -1,3 +1,4 @@
+import tqdm
 
 
 def score_solution(instance, solution):
@@ -5,34 +6,42 @@ def score_solution(instance, solution):
     duration = instance['headers']['duration']
     streets_dict = {}
     traveling_cars = {}
+    streetname_to_street = {}
+    street_to_streetname = {}
+    for index, st in streets.items():
+        name = st['name']
+        streetname_to_street[name] = index
+        street_to_streetname[index] = name
     for i in range(duration):
         traveling_cars[i] = []
-    for street in streets:
+    for street in streets.values():
         streets_dict[street['name']] = []
-    for path in instance['paths']:
+    for path in instance['paths'].values():
         beginning_street_name = path['names'][0]
         streets_dict[beginning_street_name].append(path['index'])
 
     scorer = 0
 
-    for t in range(duration):
-        for street in streets:
+    for t in tqdm.tqdm(range(duration)):
+        for street in streets.values():
             street_name = street['name']
             has_green_light = solution[t][street['index']]
             if not has_green_light:
+                continue
+            if len(streets_dict[street_name]) == 0:
                 continue
             first_car = streets_dict[street_name].pop(0)
             remaining_streets = instance['paths'][first_car]['names']
             if len(remaining_streets) == 0:
                 scorer += instance['headers']['bonus'] + (duration - t)
                 continue
-            instance['paths'][first_car]['names'].pop(0)
-            next_street = instance['paths'][first_car]['names'][0]
-
+            next_street_name = instance['paths'][first_car]['names'].pop(0)
+            next_street = streetname_to_street[next_street_name]
             traveling_time = t + instance['streets'][next_street]['cross_time']
             traveling_cars[traveling_time].append((first_car, next_street))
 
-            for car, street in traveling_cars[t]:
-                streets_dict[street].append(car)
+        for car, street in traveling_cars[t]:
+            street_name = street_to_streetname[street]
+            streets_dict[street_name].append(car)
 
     return scorer
