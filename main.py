dc1fe76d053ec12e017c74f58744340e2d4d7317
mduval <maxime@kili-technology.com>
2021-02-25 21:01:07 +0100

    Update main

--- main.py
+++ main.py
@@ -32,9 +32,9 @@ def main(solver, instance):
     # for instance, solution in list(zip(instances, solutions)):
     #     total_scores += score_solution(instance, solution)
     # print(f'Total scores: {total_scores}')
-    # print('Writing instances', end='\t')
-    # instances = write_solutions(solutions, instances)
-    # print('Done.')
+    print('Writing instances', end='\t')
+    instances = write_solutions(solutions, instances)
+    print('Done.')
 
 
 if __name__ == '__main__':
