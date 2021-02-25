1e34c8e2954bbd2353b6a7bc003de7bbdba2f0af
Paul Maisondieu <paul.maisondieu@kili-technology.com>
2021-02-25 20:14:44 +0100

    Validity v0

--- utils/valid.py
+++ utils/valid.py
@@ -1,31 +1,52 @@
 """
 Checks the submission is valid
 """
+import networkx as nx
+
+
+def intersections_dict(streets):
+    G = nx.DiGraph()
+    for street in streets.element():
+        start = street['start_intersection']
+        end = street['end_intersection']
+        length = street['cross_time']
+        G.add_edge(start, end, weight=length)
+
+    return G.nodes()
+
+
+def check_validity_T(streets, solution_T):
+
+    intersections = intersections_dict(streets)
+    validity = True
+    intersection_key = 0
+    while validity:
+
+        intersection = intersections[intersection_key]
+        number_of_green_lights = 0
+        for street in intersection:
+            light = solution_T[street]
+            number_of_green_lights += light
+
+        if number_of_green_lights > 1:
+            validity = False
+
+        intersection_key += 1
+
+    return validity
 
 
 def check_validity(instance, solution):
-    if solution['n_d'] > instance['headers']['n_pizzas']:
-        raise Exception('Number of deliveries exceeds number of pizzas')
-    if solution['n_d'] != len(solution['deliveries']):
-        raise Exception('Inconsistency in solution')
-    for team_size in [2, 3, 4]:
-        teams = instance['headers'][f't_{team_size}']
-        d_to_size = 0
-        for d in solution['deliveries']:
-            if d['team_size'] == team_size:
-                d_to_size += 1
-        if teams < d_to_size:
-            raise Exception(f'Inconsistency in solution team_size {team_size}')
-    n_pizzas = instance['headers']['n_pizzas']
-    delivered = dict(zip(list(range(n_pizzas)), [False]*n_pizzas))
-    for i, d in enumerate(solution['deliveries']):
-        for pizza in d['pizzas']:
-            if not delivered[pizza]:
-                delivered[pizza] = True
-            else:
-                raise Exception(
-                    f'Pizza {pizza} already delivered, delivery {i}')
-    for i, d in enumerate(solution['deliveries']):
-        if d['team_size'] != len(d['pizzas']):
-            raise Exception(
-                f'All members of the team should receive a pizza delivery {i}, pizzas {d["pizzas"]}, team size {d["team_size"]}')
+    D = solution.shape[0]
+    validity = True
+    T = 0
+
+    while validity and T < D:
+        validity = check_validity_T(instance, solution[T])
+        T += 1
+
+    return validity
+
+
+if __name__ == '__main__':
+    None
