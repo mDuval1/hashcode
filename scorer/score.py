8c2960c059ba2275b53f358182a2c80c1a0b8139
Pierre Marcenac <pierre@kili-technology.com>
2021-02-25 20:10:43 +0100

    Add scorer

--- scorer/score.py
+++ scorer/score.py
@@ -1,15 +1,38 @@
 
 
 def score_solution(instance, solution):
-    ds = solution['deliveries']
-    pizzas = instance['pizzas']
-    total = 0
-    for d in ds:
-        d_pizzas = d['pizzas']
-        ingredients = []
-        for id_ in d_pizzas:
-            ingredients.extend(pizzas[id_]['ingredients'])
-        n_ing = len(set(ingredients))
-        total += n_ing**2
-    print('Score for instance {0} : {1}'.format(instance['path'], total))
-    return total
+    streets = instance['streets']
+    duration = instance['headers']['duration']
+    streets_dict = {}
+    traveling_cars = {}
+    for i in range(duration):
+        traveling_cars[i] = []
+    for street in streets:
+        streets_dict[street['name']] = []
+    for path in instance['paths']:
+        beginning_street_name = path['names'][0]
+        streets_dict[beginning_street_name].append(path['index'])
+
+    scorer = 0
+
+    for t in range(duration):
+        for street in streets:
+            street_name = street['name']
+            has_green_light = solution[t][street['index']]
+            if not has_green_light:
+                continue
+            first_car = streets_dict[street_name].pop(0)
+            remaining_streets = instance['paths'][first_car]['names']
+            if len(remaining_streets) == 0:
+                scorer += instance['headers']['bonus'] + (duration - t)
+                continue
+            instance['paths'][first_car]['names'].pop(0)
+            next_street = instance['paths'][first_car]['names'][0]
+
+            traveling_time = t + instance['streets'][next_street]['cross_time']
+            traveling_cars[traveling_time].append((first_car, next_street))
+
+            for car, street in traveling_cars[t]:
+                streets_dict[street].append(car)
+
+    return scorer
